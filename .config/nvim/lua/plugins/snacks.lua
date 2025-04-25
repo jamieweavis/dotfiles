d1909baa850979fa32a2ec4942ab988c96bedbda
Jamie <jamie.weavis@gmail.com>
2025-04-25 15:18:10 +0100

    Configure snacks dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,7 +5,28 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = false },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = 'header' },
+        { section = 'keys', padding = 1 },
+        {
+          section = 'recent_files',
+          icon = ' ',
+          title = 'Recent Files',
+          indent = 2,
+          padding = 1,
+        },
+        {
+          icon = ' ',
+          title = 'Projects',
+          section = 'projects',
+          padding = 1,
+          indent = 2,
+        },
+        { section = 'startup' },
+      },
+    },
     explorer = { enabled = false },
     indent = { enabled = false },
     input = { enabled = false },

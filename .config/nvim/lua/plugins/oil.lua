d870e4ee8718b5b8853b72a5367a0c616cc2b69f
Jamie Weavis <8133259+jamieweavis@users.noreply.github.com>
2025-05-10 08:12:32 +0100

    Set oil to default file explorer

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,7 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
+    default_file_explorer = true,
     delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
     float = {

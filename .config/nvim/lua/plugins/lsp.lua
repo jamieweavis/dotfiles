7266faa72ffeadd29e6f9464c12b42bd3bb96682
Jamie Weavis <8133259+jamieweavis@users.noreply.github.com>
2025-07-09 14:19:36 +0100

    Remove deno language server

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -167,11 +167,6 @@ return {
         single_file_support = false,
         settings = {},
       },
-      denols = {
-        root_dir = require('lspconfig').util.root_pattern { 'deno.json', 'deno.jsonc' },
-        single_file_support = false,
-        settings = {},
-      },
       lua_ls = {
         settings = {
           Lua = {

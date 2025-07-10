a0811abd1e5349dc3b1b5e3250c6ceccab1e4e08
Jamie Weavis <8133259+jamieweavis@users.noreply.github.com>
2025-07-10 17:09:02 +0100

    Update nvim lazy lock and disable rust & go lsps

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -159,8 +159,8 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      gopls = {},
-      rust_analyzer = {},
+      -- gopls = {},
+      -- rust_analyzer = {},
       marksman = {},
       ts_ls = {
         root_dir = require('lspconfig').util.root_pattern { 'package.json', 'tsconfig.json' },

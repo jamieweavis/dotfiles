c0f00d12628bcd5f65804e26d3ee9142a1390e35
Jamie Weavis <8133259+jamieweavis@users.noreply.github.com>
2026-09-08 16:52:00 +0100

    Remove tpm

--- install.sh
+++ install.sh
@@ -13,12 +13,6 @@ fi
 echo "● Installing Brewfile formulae & casks"
 brew bundle --file=./.config/brew/Brewfile
 
-echo "● Installing tpm"
-[[ ! -d ~/.tmux/plugins/tpm ]] && install_tpm=true
-if [ $install_tpm ]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
 # (2/4) Symlink dotfiles
 echo "● Symlinking dotfiles"
 stow . --verbose 2 --adopt --target ~
@@ -26,13 +20,6 @@ stow . --verbose 2 --adopt --target ~
 # (3/4) Install plugins
 echo "● Installing nvim plugins"
 nvim --headless "+Lazy! sync" +qa
-
-echo "● Installing tpm plugins"
-if [ $install_tpm ]; then
-	~/.tmux/plugins/tpm/bin/install_plugins
-else
-	~/.tmux/plugins/tpm/bin/update_plugins all
-fi
 
 # (4/4) Finally source ~/.zshrc
 echo "\n● Installation complete - sourcing ~/.zshrc 🚀"
